17f3543b2ef9bd27821f7e23738bf2c784cd8326
Hauke Schade <cnlpete@cnlpete.de>
2013-08-30 19:41:40 +0200

    screen reader support for the search field

--- searchform.php
+++ searchform.php
@@ -1,5 +1,6 @@
 <form method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>">
 	<div class="input-group">
+		<label class="sr-only" for="s"><?php _e('Search', 'responsivepanels'); ?></label>
 		<input type="text" class="form-control" name="s" placeholder="<?php _e('Search', 'responsivepanels'); ?>" id="s" />
 		<span class="input-group-btn">
 			<button type="button" id="searchsubmit" class="btn btn-default"><?php _e('Search', 'responsivepanels'); ?></button>
